49f39e3a6449a6c48ff462e87e2b51d57d2a7d31
Zilde Souto Maior Neto <37539515+zsmn@users.noreply.github.com>
2019-03-19 16:45:56 -0300

    Identação + Comentários

--- list_1/q4.asm
+++ list_1/q4.asm
@@ -1,73 +1,83 @@
-#comentar dps
-
 .data
-str: .asciiz "lUzAZu  L"
+	str: .asciiz "lUzAZu  L"
 
 .text
 	addi a0, zero, 1
-  addi a1, zero, 0
-  addi a2, zero, 0
-  addi a3, zero, 32
-  addi a4, zero, -32
+	addi a1, zero, 0
+	addi a2, zero, 0
+ 	addi a3, zero, 32
+ 	addi a4, zero, -32
 	addi a7, zero, 0
     
-  addi s4, zero, 2
-  addi s5, zero, 1  
+    addi s4, zero, 2
+	addi s5, zero, 1  
     
-  la s1, str
-  la s2, str
+	la s1, str
+	la s2, str
     
-contarcaracteres:
+contar_caracteres:
+	# Realiza a contagem da quantia de caracteres presentes na string #
 	lb s3, (0)s1
-  beq s3, a2, check_palindrome # se for igual ao fim da str, manda pra o check_palindrome
-  addi s1, s1, 1 # mover o cursor pelos caracteres da str
-  addi a7, a7, 1
-  j contarcaracteres
+    beq s3, a2, calcular_termos
+    addi s1, s1, 1
+	addi a7, a7, 1
+j contar_caracteres
 
-check_palindrome:
+calcular_termos:
 	div a5, a7, s4
-  addi a5, a5, 1
-  sub a6, a7, a5
-  sub s1, s1, s5
-  j brincadeira
+    addi a5, a5, 1
+	sub a6, a7, a5
+	sub s1, s1, s5
+j checar_palindromo
 
-brincadeira:
-	lb s3, (0)s1 #s3 ta começando de trás
-  lb s6, (0)s2 #s6 ta na frente
-  beq s9, a5, fim
-  beq s10, a6, fim
+checar_palindromo:
+	# A ideia é que 's3' tem o ponteiro no fim da string e 's6' um ponteiro no inicio #
+    # dessa forma, podemos comparar as pontas e analisar mais facilmente o palindromo #
+	lb s3, (0)s1
+    lb s6, (0)s2
+    # Caso tenha chegado no limite da string que está sendo analisada a esquerda #
+	beq s9, a5, fim 
+    # Caso tenha chegado no limite da string que está sendo analisada a direita #
+	beq s10, a6, fim
+    # Caso não sejam caracteres iguais, testam-se os casos que possam gerar a diferença #
 	bne s3, s6, tratador
     
-  addi s9, s9, 1
-  addi s10, s10, 1
+    # Adiciona +1 nos contadores de iteração #
+	addi s9, s9, 1
+	addi s10, s10, 1
+    # Movimenta os ponteiros das strings, um para a esquerda e outro para a direita #
+	sub s1, s1, s5
+	addi s2, s2, 1
     
-  sub s1, s1, s5
-  addi s2, s2, 1
-    
-  j brincadeira
+j checar_palindromo
     
 tratador:
+	# Armazenamos em 's7' a diferença entre os caracteres #
 	sub s7, s3, s6
-  beq s7, a3, caso1 #caso onde s3 é maiusculo e s6 eh minusculo
-  beq s7, a4, caso1 #caso onde s3 é minusculo e s6 eh maiusculo
-  beq s3, a3, caso2 #caso onde s3 (direita) tem um espaco
-  beq s6, a3, caso3 #caso onde s6 (esquerda) tem um espaco
-  sub a0, a0, s5
-  j fim
+    # Caso a diferença seja, em módulo, 32, indica que um está em uppercase e o outro em lowercase #
+	beq s7, a3, caso_lower_up
+	beq s7, a4, caso_lower_up
+    # Caso seja encontrado um espaço no ponteiro do registrador 's3' #
+	beq s3, a3, caso_esp_s3 #caso onde s3 (direita) tem um espaco
+    # Caso seja encontrado um espaço no ponteiro do registrador 's6' #
+	beq s6, a3, caso_esp_s6 #caso onde s6 (esquerda) tem um espaco
+    # Caso não seja nenhum dos casos acima, então realmente os caracteres são diferentes, logo não é palindromo #
+	sub a0, a0, s5
+j fim
 
-caso1:
+caso_lower_up:
 	sub s1, s1, s5
-  addi s2, s2, 1
-  j brincadeira
+	addi s2, s2, 1
+j checar_palindromo
 
-caso2:
+caso_esp_s3:
 	sub s1, s1, s5
-  addi s10, s10, 1
-  j brincadeira
+	addi s10, s10, 1
+j checar_palindromo
     
-caso3:
-  addi s2, s2, 1
-  addi, s9, s9, 1
-  j brincadeira
+caso_esp_s6:
+	addi s2, s2, 1
+	addi, s9, s9, 1
+j checar_palindromo
 
 fim:
